af5f7a6fbfcb6573d724bd8899fb11cbd51c9d74
Noor-26 <nk26982698@gmail.com>
2022-03-26 00:06:26 +0600

    componenets added and linked

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Shop from './components/Shop/Shop';
 
 function App() {
   return (
     <div className="App">
       <h1>Darkfam book shop</h1>
+      <Shop></Shop>
     </div>
   );
 }
---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,18 @@
+import Books from '../Books/Books';
+import './Shop.css'
+
+
+const Shop = () => {
+    return (
+        <div className="shop-container">
+            <div className="book-container ">
+                <h1>book container</h1>
+            </div>
+            <div className="select-container">
+                <h1>select books</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
