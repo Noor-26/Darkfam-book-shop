75002bd161fc167df35fde544b17a1cfbcfc3d9b
Noor-26 <nk26982698@gmail.com>
2022-03-26 00:22:21 +0600

    books card added and borders removed off book and select containers

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,5 +1,7 @@
 import  { useState,useEffect } from 'react';
-import './Shop.css';
+import Books from '../Books/Books';
+import './Shop.css'
+
 
 const Shop = () => {
     const [books, setbooks] = useState([])
@@ -10,8 +12,11 @@ const Shop = () => {
       }, [books])
     return (
         <div className="shop-container">
-            <div className="book-container ">
+            <div className="book-container row row-cols-1 row-cols-md-3 g-4">
                 <h1>book container</h1>
+            {
+            books.map(book => <Books data={book}></Books>)
+            }
             </div>
             <div className="select-container">
                 <h1>select books</h1>
